06d99fd0f06f95b6cac0b40ebc04450aef850661
Julian Welter de Jesus <julianwelterdejesus@gmail.com>
2023-03-16 00:09:59 -0300

    adicionado implementação do desafio fabrica de carro

--- src/main/java/me/dio/web/desafio/algoritmosBasicos/fabricaDeCarros/FabricaDeCarros.java
+++ src/main/java/me/dio/web/desafio/algoritmosBasicos/fabricaDeCarros/FabricaDeCarros.java
@@ -1,2 +1,25 @@
-package me.dio.web.desafio.algoritmosBasicos.fabricaDeCarros;public class FabricaDeCarros {
+package me.dio.web.desafio.algoritmosBasicos.fabricaDeCarros;
+
+import java.util.Scanner;
+
+public class FabricaDeCarros {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int custoFabrica = scan.nextInt();
+        int porcentagemDistribuidor = scan.nextInt();
+        int PercentualImpostos = scan.nextInt();
+
+        int custoConsumidor;
+
+        int distribuidor;
+        int valorImpostos;
+
+        // TODO: Implemente uma condição que calcule a porcentagem do distribuidor e dos impostos:
+
+       distribuidor = (custoFabrica * porcentagemDistribuidor) / 100;
+
+
+        System.out.println(distribuidor);
+    }
 }
